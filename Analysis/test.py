fd53cdbffb1de0bce0b0ec4fdd616ab22d4c3066
Jiajun <shenjiajun90@gmail.com>
2014-10-26 01:07:43 -0500

    test file

--- Analysis/test.py
+++ Analysis/test.py
@@ -1,8 +1,34 @@
 from WeatherStationReader import WeatherStationReader
+from WeatherReader import WeatherReader
 
 reader = WeatherStationReader('../Data/isd-history.csv')
-print(len(reader._stationList))
+#print(len(reader._stationList))
 
-for i in reader._stationList:
-    i._nearestStations(reader._stationList,neighborNum = 20)
-    print(len(i._nearbyStationList))
+i = reader._stationList[2000]
+#print(i._LON,i._LAT)
+i._nearestStations(reader._stationList,radius = 10)# 10 here is the length of radius.
+#print(len(i._nearbyStationList))
+
+lon = reader._stationList[2000]._LON
+lat = reader._stationList[2000]._LAT
+#print(lon,lat)
+#print(reader._pointNearestStations(lat,lon,radius = 1000))
+lon = lon - 1
+lat = lat -1
+a,b,c = reader.getWeightsBasedOnLocation(lat,lon,radius = 1000)
+
+print(c)
+
+
+reportReader = WeatherReader('../Data/ChicagoWeather.csv')
+reportReader._weatherReportList[1].printOut()
+
+wban_code_list = ["94846","4807","14819"]
+
+print(wban_code_list)
+import datetime
+reportTime = datetime.datetime.strptime("2014-01-31T23:49:00", "%Y-%m-%dT%H:%M:%S")
+reportList = reportReader.retrieveWeatherReports(wban_code_list,reportTime)
+
+for x in reportList:
+    x.printOut()
