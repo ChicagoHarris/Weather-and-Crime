ecbe857b5ebf87d435948bc13128bf1f6193f740
Jiajun <shenjiajun90@gmail.com>
2015-05-22 10:40:56 -0500

    update

--- src/updateNN.R
+++ src/updateNN.R
@@ -10,6 +10,7 @@
         quit()
     }
 
+    ##Specifying the input parameters
     print("======crime type========")
     print(args[1])
     crimeType = args[1]
@@ -26,7 +27,7 @@
     print(args[4])
     indexOfBaggedSamples = as.integer(args[4])
 
-    print("=====Number of Iterations============")
+    print("=====Number of Updating Iterations============")
     print(args[5])
     nIter = as.integer(args[5])
 
@@ -35,26 +36,33 @@
     dateTime = args[6]
 
 
-    #load updated training data
+    #load updated training data. The updated training data is the combination of historical bagged data and the updated bagged data
     print("loading training data...")
     trainingData = readRDS(file = paste(updateDataDir, "./bagTrainingData_", indexOfBaggedSamples, ".rds", sep = ""))
 
     print("Done")
 
-    #load trained NN
-    trainedNN = readRDS(file = paste(modelDir, "/._NNmodel_", indexOfBaggedSamples))
+    #load trained NN. The based model we trained previously. Filename Example: ._NNmodel_1
+    trainedNN = readRDS(file = paste(modelDir, "/._NNmodel_", indexOfBaggedSamples, ""))
     
+
+    #Get the number of hidden nodes of the trained NN
     nHidden = trainedNN$n[2]
+
+    #Get the model formula to prepare to update the new NN.
     crimeTypeColumnName = c("shooting_count", "robbery_count", "assault_count")
     names = names(trainingData)
     f <- as.formula(paste(paste(crimeType," ~"), paste(names[!names %in% crimeTypeColumnName], collapse = " + ")))
     
-    #Update NN model
+
+
+    #Update NN model; Key trick here it to specify "Wts = trainedNN$wts". This allow us to extract the weight parameter trained in the previous NN and start updating weights from there.
+    # Thus, we only need to update the model for a small number of interations instead of running another 200 iterations.
 
     ir.nn = nnet(f, data = trainingData, size = nHidden, rang = 0.1, MaxNWts = 30000, decay = 5e-4, maxit = nIter, Wts = trainedNN$wts)
 
 
-    #Save trained model in model directory
+    #Save updated model in model directory
     print("Saving model.....")
     saveRDS(ir.nn, file = paste(modelDir, "/._NNmodel_", indexOfBaggedSamples, "_Update_", dateTime, ".rds", sep = ""))
 
