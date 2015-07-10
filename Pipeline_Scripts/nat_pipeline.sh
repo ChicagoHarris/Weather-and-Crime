24122010c687767434e31b343735d7d8aa09bc73
natnaell <natnaell@gmail.com>
2015-07-10 08:25:24 -0400

    Run predictions in parallel

--- Pipeline_Scripts/nat_pipeline.sh
+++ Pipeline_Scripts/nat_pipeline.sh
@@ -4,6 +4,8 @@ bash GetUpdateWeatherData
 bash GetUpdateMetarData
 #Pull forecast data from noaa
 python forecastScraper.py
+cp forecasts.csv Weather_Forecasts.csv
+mv Weather_Forecasts.csv ~/Dropbox/Public/Mockup\ CSV\ Folder/
 
 #Merge and reformat plenario metar and qcd data and noaa forecast data.
 python reformat_plenario_weather.py
@@ -12,14 +14,13 @@ cd LagBin
 
 #Bin lagged_forecats.csv; output is binned_forecasts.csv
 bash bag_and_bin_prediction_pipeline.sh
-cp forecasts.csv Weather_Forecasts.csv
-mv Weather_Forecasts.csv ~/Dropbox/Public/Mockup\ CSV\ Folder/
 mv binned_forecasts.csv ..
 cd ..
 #Create robbery predictions
-Rscript testNN_robbery_edited.R "robbery" robbery/binned_csv/ robbery/ output/ 100 binned_forecasts.csv
-Rscript testNN_robbery_edited.R "assault" assault/binned_csv/ assault/ output/ 100 binned_forecasts.csv
-Rscript testNN_robbery_edited.R "shooting" shooting/binned_csv/ shooting/ output/ 100 binned_forecasts.csv
+Rscript testNN_robbery_edited.R "robbery" robbery/binned_csv/ robbery/ output/ 100 binned_forecasts.csv &
+Rscript testNN_robbery_edited.R "assault" assault/binned_csv/ assault/ output/ 100 binned_forecasts.csv &
+Rscript testNN_robbery_edited.R "shooting" shooting/binned_csv/ shooting/ output/ 100 binned_forecasts.csv &
+wait
 cd output
 python add_id.py
 mv Crime\ Prediction\ CSV\ MOCK\ -\ revised.csv ~/Dropbox/Public/Mockup\ CSV\ Folder/
