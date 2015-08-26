7c43797e720062a3812ffac187eebfd3fc70cb4a
Jiajun Shen <shenjiajun90@gmail.com>
2015-08-26 13:25:04 -0500

    Rm

--- Updater/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-test=`date --date="$(date +%Y-%m-%d) -1 month" +%m`
-echo $test
-
--- Updater/test2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in `seq 1 100`
-
-do
-echo $i
-Rscript updateNN_NoWeather.R robbery_count /mnt/research_disk_1/newhome/weather_crime/Weather-and-Crime/Master_Pipeline/robbery/binned_csv/ /mnt/research_disk_1/newhome/weather_crime/Weather-and-Crime/Master_Pipeline/no_weather_robbery/ $i 5 2015-07-06
-
-done 
