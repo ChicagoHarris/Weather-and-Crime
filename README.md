d1b15fa35f5701bb1b549815e270226f046ca280
Kyle Magida <kmagida@uchicago.edu>
2015-11-04 21:23:14 -0600

    Updated ReadME to initialization

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Crime data is from the City of Chicago. Weather data is from NOAA, via the Plena
 
 This project is funded by a grant from the Arnold Foundation and is housed at the University of Chicago.
 
+DEPENDENCIES:
+Run CrimeScapeInitializer.sh to install these
+
+- Postgres (installed in manual_initialization.txt)
+- Postgis
+- jg
+- go
+- jsontocsv from github.com/jehiah/json2csv
+- R
+
+DATABASE & R INITIALIZATION:
+
+The database will need to be initialized once using postgres. Instructions on how to this are found in manual_initialization.txt. This file will need to be run line by line in the terminal as output will need to be adjusted based on the output from the installing scripts. Usernames and passwords will also need to be set manually in postgres.
+
+
+
+
--- Updater/CrimeScapeInitializer.sh
+++ Updater/CrimeScapeInitializer.sh
@@ -1,14 +1,17 @@
 # CrimeScape Initializer
-
-
 # For Updater Pipeline
 # Run from project's Updater directory: Weather-and-Crime/Updater/
 
 
-## For Getting the data
+## For Getting the data and installing R
 brew install jq
 brew install wget
 brew install go
+brew tap homebrew/science
+brew install gcc
+brew install Caskroom/cask/xquartz
+brew install r
+
 
 # set GOPATH
 mkdir $HOME/work
@@ -19,45 +22,12 @@ go get github.com/jehiah/json2csv
 
 ## For Formatting the data
 
-#postgres
-brew install postgresql
 #postgis
 brew install postgis
 
 
 # Start postgres on OS X
-launchctl load /usr/local/Cellar/postgresql/9.4.4/homebrew.mxcl.postgresql.plist
 
-# If you get an error that role "postgres" does not exist, run: 
-psql postgres
-# In postgres run:
-create role postgres;   #Sets "postgres" as your username
-\password postgres
-# Enter desired password.  #Sets your password for username "postgres". 
-#Username and password set here are used in lines 42 and 49 below. 
-\q # to exit postgres
-
-# Create database credentials with following syntax: 
-#echo "127.0.0.1:5432:postgres:username:password" > .pgpass
-echo "127.0.0.1:5432:postgres:postgres:postgres" > .pgpass
-
-# Create database
-psql postgres < databaseInit.sql
-
-# Update database credentials to use our new database with following syntax:
-#echo "127.0.0.1:5432:weatherandcrime:username:password" > .pgpass
-echo "127.0.0.1:5432:weatherandcrime:postgres:postgres" > .pgpass
-
-# Enable spatial functions in our database
-postGisScriptDir="/usr/local/Cellar/postgis/2.1.7_1/share/postgis"
-psql -f "$postGisScriptDir/postgis.sql" weatherandcrime
-psql -f "$postGisScriptDir/spatial_ref_sys.sql" weatherandcrime
-
-# Import Shapefile 
-# Requires postgis to run shp2pgsql
-# and requires Census_Tracts.shp to be in directory.
-shp2pgsql -s 3435 Census_Tracts/Census_Tracts.shp public.censustracts weatherandcrime > censustracts.sql
-psql weatherandcrime < censustracts.sql
 
 # R updater section
 R
